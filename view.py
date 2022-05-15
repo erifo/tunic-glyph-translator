c0a8489d7e24c2791133bef4effcbf3e44f4c731
Eric Forsberg <eric.forsberg.work@gmail.com>
2022-05-15 16:46:50 +0200

    Added shaded "off" version of segments.

--- view.py
+++ view.py
@@ -5,7 +5,8 @@ class View:
     def __init__(self, width, height) -> None:
         self.WIDTH = width
         self.HEIGHT = height
-        self.COLOR = (255,200,50)
+        self.ACTIVE_COLOR = (255,200,50)
+        self.INACTIVE_COLOR = (40,40,40)
         display.set_caption('TUNIC Glyph Translator')
         self.screen = display.set_mode((self.WIDTH, self.HEIGHT))
         self.segments = []
@@ -14,24 +15,36 @@ class View:
         # Background
         self.screen.fill((0,0,0))
 
-        # Flipcircle
+        # Flipcircle OFF
+        c = model.flipcircle
+        draw.circle(self.screen, self.INACTIVE_COLOR, (c.x1, c.y1), c.radius, width=10)
+
+        # Line Segment OFF
+        for seg in model.segments:
+            start = (seg.x1, seg.y1)
+            end = (seg.x2, seg.y2)
+            draw.line(self.screen, self.INACTIVE_COLOR, start, end, width=14)
+            draw.circle(self.screen, self.INACTIVE_COLOR, (seg.x1+1, seg.y1+1), 7)
+            draw.circle(self.screen, self.INACTIVE_COLOR, (seg.x2+1, seg.y2+1), 7)
+        
+        # Flipcircle ON
         c = model.flipcircle
         if (model.state & c.bin == c.bin):
-            draw.circle(self.screen, self.COLOR, (c.x1, c.y1), c.radius, width=5)
+            draw.circle(self.screen, self.ACTIVE_COLOR, (c.x1, c.y1), c.radius, width=10)
 
-        # Line Segments
+        # Line Segments ON
         for seg in model.segments:
             if (model.state & seg.bin != seg.bin):
                 continue
             start = (seg.x1, seg.y1)
             end = (seg.x2, seg.y2)
-            draw.line(self.screen, self.COLOR, start, end, width=14)
-            draw.circle(self.screen, self.COLOR, (seg.x1+1, seg.y1+1), 7)
-            draw.circle(self.screen, self.COLOR, (seg.x2+1, seg.y2+1), 7)
+            draw.line(self.screen, self.ACTIVE_COLOR, start, end, width=14)
+            draw.circle(self.screen, self.ACTIVE_COLOR, (seg.x1+1, seg.y1+1), 7)
+            draw.circle(self.screen, self.ACTIVE_COLOR, (seg.x2+1, seg.y2+1), 7)
         
         # Translated Text
         myfont = font.Font('freesansbold.ttf', 30)
-        text = myfont.render(str(model.getWord()), True, self.COLOR, (50,50,50))
+        text = myfont.render(str(model.getWord()), True, self.ACTIVE_COLOR, (50,50,50))
         textRect = text.get_rect()
         textRect.center = (self.WIDTH//2, self.HEIGHT-25)
         self.screen.blit(text, textRect)
